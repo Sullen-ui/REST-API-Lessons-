fb6d9ee6e0b93e9e5f277cfe4a68623528b4d8ae
Sullen-ui <maorlov.it@gmail.com>
2021-04-27 22:33:37 +0600

    create view "Welcome", route & ArticlesController - Method

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\ArticlesController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// GET- запросы
+// Получение постов из таблицы Articles
+Route::get('/articles',[ArticlesController::class, "showArticles"]);  
