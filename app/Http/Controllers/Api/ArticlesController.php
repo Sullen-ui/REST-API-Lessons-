f2ff6ad4fcfeba2d6374a960001ad736ec4188fb
Sullen-ui <maorlov.it@gmail.com>
2021-04-27 22:40:18 +0600

    showArticles

--- app/Http/Controllers/Api/ArticlesController.php
+++ app/Http/Controllers/Api/ArticlesController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
     public function showArticles()
     {
-        
+         return Article::all();
     }
 }
